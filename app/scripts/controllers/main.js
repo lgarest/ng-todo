a8cb9888c681d5dd02f6dca6ceeef563bef2bad4
lgarest <luisgarciaestrades@gmail.com>
2015-04-19 21:37:43 +0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the todoApp
+ */
+angular.module('todoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Sitepoint',
+    ];
+
+  });
