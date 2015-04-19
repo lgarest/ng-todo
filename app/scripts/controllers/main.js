c17ac9324af8a028bd4680777d23e3ea13bcfa55
lgarest <luisgarciaestrades@gmail.com>
2015-04-20 00:26:28 +0200

    First version of the todo list, *gitignore

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,37 @@
  */
 angular.module('todoApp')
   .controller('MainCtrl', function ($scope) {
+    $scope.showDone = true;
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma',
       'Sitepoint',
     ];
+
+    $scope.model = {
+        user: 'Luis',
+        items: [
+            {action: 'Buy Flowers', done: false},
+            {action: 'Go to sleep', done: false},
+            {action: 'set conf directory', done: true},
+        ],
+    };
+
+    $scope.incompleteCount = function(){
+        var count = 0;
+        angular.forEach($scope.model.items, function(item){
+            if (!item.done) { count++; }
+        });
+        return count;
+    };
+
+    $scope.warningLevel = function(){
+        return $scope.incompleteCount() < 3 ? 'label-success' : 'label-warning';
+    };
+
+    $scope.addNewItem = function(actionText){
+        $scope.model.items.push({action: actionText, done:false});
+    };
 
   });
